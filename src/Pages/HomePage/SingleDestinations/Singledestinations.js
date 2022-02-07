f231e7a13b72914a4e8bd95913407a437fffa50b
Safayet Mahid <safayetmahid96@gmail.com>
2022-02-07 21:15:22 +0600

    updating

--- src/Pages/HomePage/SingleDestinations/Singledestinations.js
+++ src/Pages/HomePage/SingleDestinations/Singledestinations.js
@@ -12,6 +12,9 @@ const Singledestinations = (props) => {
                 <div className='destination' style={{ backgroundImage: `url(${image1})` }}>
 
                     <div className='destination-body' style={{ background: `linear-gradient(180deg,rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.92)) 100% ` }}>
+                        <div className='onHover'>
+                            <p className='text-xl font:medium'>Click for details</p>
+                        </div>
                         <h4 className='font-extrabold text-5xl '>{town_name}</h4>
                         <h5 className='font-medium text-lg mt-2'>{state_name}</h5>
                     </div>
